82232dab9c31cb0f077fa0c7d006a61e7f0f7923
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2025-04-19 00:13:17 +0200

    input field added and styled

--- src/app/main-content/home/home.component.ts
+++ src/app/main-content/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
