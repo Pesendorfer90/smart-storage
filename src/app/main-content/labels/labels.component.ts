25209d0e7a23225096d28dfad7ce8cd332b3ce71
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2025-05-20 18:29:26 +0200

    labels logic added, add, update, delete labels, connected to frestore

--- src/app/main-content/labels/labels.component.ts
+++ src/app/main-content/labels/labels.component.ts
@@ -1,13 +1,17 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, WritableSignal } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { FormsModule } from '@angular/forms';
-import { MatChipsModule, MatChipInputEvent,  MatChipEditedEvent} from '@angular/material/chips';
+import { MatChipsModule, MatChipInputEvent, MatChipEditedEvent } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Labels } from '../../models/labels';
+import { FirestoreService } from '../../service/firestore.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { collection, doc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-lables',
@@ -25,19 +29,25 @@ import { Labels } from '../../models/labels';
   styleUrl: './labels.component.scss'
 })
 export class LablesComponent {
-  readonly labels = signal<Labels[]>([{name: 'angular', id: 'asd54g45g'}, {name: 'how-to', id: 'dfgh9kjm6785'}, {name: 'tutorial', id: '51gf6'}, {name: 'accessibility', id: 'dfg564564gf'}, {name: "Drink", id: '347gzu5ufg'}, {name: "Food", id: 'mpkmxcs45'}, {name: "Werkzeug", id: 'odxgwkl5'}, {name: "Alkohol", id: 'mxölwkjt56'}, {name: "Wein", id: 'odvcnmsl5'}, {name: "2022", id: 'fm549308rhe'}]);
+  labels: WritableSignal<Labels[]> = signal<Labels[]>([]);
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  readonly announcer = inject(LiveAnnouncer);
 
   edit: boolean = false;
 
-  announcer = inject(LiveAnnouncer);
+  constructor(private firestoreService: FirestoreService) {
+    this.firestoreService.labels$.subscribe(data => {
+      this.labels.set(data);
+    });
+  }
 
-  removeLabel(keyword: Labels) {
+  async removeLabel(keyword: Labels) {
+    await this.firestoreService.deleteLabel(keyword.id);
     this.labels.update(keywords => {
       const index = keywords.indexOf(keyword);
-      if (index < 0) {
-        return keywords;
-      }
-
+      // if (index < 0) {
+      //   return keywords;
+      // }
       keywords.splice(index, 1);
       this.announcer.announce(`removed ${keyword.name} from template form`);
       return [...keywords];
@@ -48,10 +58,11 @@ export class LablesComponent {
     const value = (event.value || '').trim();
 
     if (value) {
-      this.labels.update(keywords => [...keywords, {name: value, id:''}]);
+      const newId = this.firestoreService.generateId('labels');
+      this.labels.update(labels => [...labels, { name: value, id: newId }]);
     }
 
-    event.chipInput!.clear();
+    event.chipInput?.clear();
   }
 
   editLabel(keyword: any, event: MatChipEditedEvent) {
@@ -71,4 +82,13 @@ export class LablesComponent {
       return keywords;
     });
   }
+
+  async saveLabels() {
+    this.edit = false
+    await this.firestoreService.saveOrUpdateLabels(this.labels())
+
+  }
+
+
+
 }
--- src/app/service/firestore.service.ts
+++ src/app/service/firestore.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { addDoc, collection, collectionData, doc, Firestore, updateDoc } from '@angular/fire/firestore';
-import { map, Observable } from 'rxjs';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, setDoc, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { Inventory } from '../models/inventory';
 import { Labels } from '../models/labels';
 import { Room } from '../models/room';
@@ -20,6 +20,8 @@ export class FirestoreService {
     this.rooms$ = this.getData<Room>('rooms');
   }
 
+  // Items & Rooms
+
   getData<T>(collectionName: string): Observable<(T & { id: string })[]> {
     const userProfileCollection = collection(this.firestore, collectionName);
     return collectionData(userProfileCollection, { idField: 'id' }) as Observable<(T & { id: string })[]>;
@@ -33,5 +35,26 @@ export class FirestoreService {
   updateImage(docId: string, photoURL: string, collectionName: string) {
     const itemDoc = doc(this.firestore, collectionName, docId);
     return updateDoc(itemDoc, { photoURL });
+  }
+
+  // Labels
+
+  async saveOrUpdateLabels(labels: Labels[]) {
+    const labelsRef = collection(this.firestore, 'labels');
+
+    for (const label of labels) {
+      const id = label.id || doc(labelsRef).id;
+      await setDoc(doc(this.firestore, 'labels', id), { name: label.name });
+    }
+  }
+
+  deleteLabel(labelId: string): Promise<void> {
+    const labelDoc = doc(this.firestore, 'labels', labelId);
+    return deleteDoc(labelDoc);
+  }
+
+  generateId(collectionName: string): string {
+    const colRef = collection(this.firestore, collectionName);
+    return doc(colRef).id;
   }
 }
